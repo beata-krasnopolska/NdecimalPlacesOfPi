d5fc6ad5c27589a15d10002a5b285f3205a60c8d
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-04 21:45:19 +0100

    Add project files.

---
+++ NplacesOfPi/DecimalFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NplacesOfPi
+{
+    class DecimalFinder
+    {
+        const double _pi_value = Math.PI;
+        public double Find(int n)
+        {
+            var deci = Math.Round(_pi_value, n);
+            return deci;
+        }
+    }
+}
---
+++ NplacesOfPi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NplacesOfPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program returns PI to n decimal places. ");
+            
+            Console.WriteLine("Enter the number of n decimal places of PI you would like to see");
+            int decimalPlaces = int.Parse(Console.ReadLine());
+
+            var nDecimalPlaces = new DecimalFinder();
+            nDecimalPlaces.Find(decimalPlaces);
+
+            Console.WriteLine(nDecimalPlaces);
+
+            Console.ReadKey();
+        }
+    }
+}
